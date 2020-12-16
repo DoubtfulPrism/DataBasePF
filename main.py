fae437c9e9c22ed725d22eaa1d30471ae6cb0b16
Doug <dougbuck@softwaretestingservices.co.uk>
2020-12-16 07:42:30 +0000

    c

---
+++ main.py
@@ -0,0 +1,12 @@
+"""
+A Program to store Pathfinder items with purchase and sale prices
+"""
+
+from tkinter import *
+
+window = Tk()
+
+l1 = Label(window, test="Name")
+l1.grid(row=0, column=0)
+
+window.mainloop()
